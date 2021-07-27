5bff0db8254249ac1f15df56feb32e933fc3d6e5
Jim Bengtsson <jim@workmobile.se>
2021-07-27 14:45:30 +0200

    Add code to see problem

---
+++ test/test_controller_test.dart
@@ -0,0 +1,10 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito_getxcontroller_test/test_controller_to_mock.dart';
+import 'package:test/test.dart';
+
+@GenerateMocks([TestControllerToMock])
+void main() {
+  group('tests TestControllerToMock', () {
+    test('test TestControllerToMock', () {});
+  });
+}
